2c883e3f6bb5c4325c282eab1b16c8268e5d0a90
VladimirPoplavsky <vovik.ds@gmail.com>
2024-01-22 11:37:02 +0200

    add note "Works only with Israeli phone numbers"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    const isValidPhoneNumber = (number) => {
+        // Regex to validate the format 0581234567 (Israeli phone number format)
+        const regex = /^\d{10}$/;
+        return regex.test(number);
+    };
+
+    const sendMessage = () => {
+        if (isValidPhoneNumber(phoneNumber)){
+            let cleanedNumber = phoneNumber.substring(2, 10)
+            const whatsappUrl = `https://api.whatsapp.com/send/?phone=%2B9725${cleanedNumber}&text&app_absent=0`;
+            window.open(whatsappUrl, '_blank');
+        } else{
+            alert("Incorrect phone number, check your input");
+        }
+    };
+
+    const onPressEnter = (e) => {
+        if (e.key === 'Enter'){
+            sendMessage();
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h2>WhatsApp Message Sender</h2>
+                <p>Note: Works only with Israeli phone numbers</p>
+                <input
+                    type="text"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    onKeyPress={onPressEnter}
+                    placeholder="Enter phone number"
+                />
+                <button onClick={sendMessage}>
+                    <i className="fa-brands fa-whatsapp"></i>
+                    Send Message
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
